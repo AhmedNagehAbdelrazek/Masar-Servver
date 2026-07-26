d1a03bbbef81d2df638042bc0f86960e88d11dd6
Ahmed Nageh <ahmednagh2005@gmail.com>
2026-07-26 20:18:29 +0300

    add aduit service

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const morgan = require('morgan');
 require('./Models/index');
 const apiRouter = require('./Routes/index');
 const globalErrorHandler = require('./middlewares/globalErrorHandler');
+const { createAuditMiddleware } = require('./external packages/audit-client');
+const { audit } = require('./config/audit');
 
 function createApp() {
   const app = express();
 
   app.use(cors());
   app.use(helmet());
+  app.use(createAuditMiddleware(audit, {
+    skip: (req) => req.path === '/health'
+  }));
   app.use(morgan('short'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
