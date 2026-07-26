a90b46943e7a7cdf342f0e55ae09b0f685e75d27
Ahmed Nageh <ahmednagh2005@gmail.com>
2026-07-26 22:35:31 +0300

    fix audit docker file

--- Main Server/config/audit.js
+++ Main Server/config/audit.js
@@ -9,6 +9,11 @@ const audit = new AuditClient({
   clientSecret: process.env.AUDIT_CLIENT_SECRET,
 });
 
+console.log(process.env.AUDIT_SERVICE_ID);
+console.log(process.env.AUDIT_COLLECTOR_URL);
+console.log(process.env.AUDIT_CLIENT_KEY);
+console.log(process.env.AUDIT_CLIENT_SECRET);
+
 const auditedFetch = createAuditedFetch(audit);
 
 exports.audit = audit;
